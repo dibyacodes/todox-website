b7f05715a593ef5fe9e2013e41a89f39f0f2d1cc
Dibyajyoti Kumar <dibyacodes@gmail.com>
2025-01-18 09:52:37 +0530

    feat : adding custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-function App() {
+import { useEffect, useState } from "react"
 
+function App() {
+  
   return (
     <>
-      <div className="text-9xl">
-        React is on
+      <div className="bg-black h-screen">
+        <div className="p-3">
+          <h1 className="text-white text-5xl max-w-[25ch] font-inter">
+            Greetings User! Welcome to todox
+          </h1>
+          <div className="flex justify-around items-center">
+            <div className="text-white font-inter text-3xl">
+              Tasklist
+            </div>
+            <div>
+              <button className="text-white bg-gray-800 px-4 py-2 rounded-sm">
+                Create New +
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'inter' : ['inter','sans-serif']
+    },
     extend: {},
   },
   plugins: [],
